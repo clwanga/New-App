a06e723a4d43950557d86d022a0d89ff2fa369d0
Charles Lwanga <clwanga1095@gmail.com>
2023-08-24 21:07:46 +0300

    learned about databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlert } from './WarningAlert/warning-alert.component';
+import { SuccessAlert } from './success-alert/success.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
     ServersComponent,
-    WarningAlert
+    WarningAlert,
+    SuccessAlert
   ],
   imports: [
     BrowserModule,
