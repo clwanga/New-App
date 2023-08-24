700626cd2b6aa51f6baef9e9ccf125984473e218
Charles Lwanga <clwanga1095@gmail.com>
2023-08-24 18:49:13 +0300

    created my first component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+import { WarningAlert } from './WarningAlert/warning-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningAlert
   ],
   imports: [
     BrowserModule,
